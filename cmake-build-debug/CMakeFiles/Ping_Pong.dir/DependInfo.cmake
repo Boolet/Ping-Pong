94edd334b974a866689efcb2b059798dc3dea9a5
Matt Sparks <masparks@uci.edu>
2018-02-12 15:33:03 -0800

    Updated HTML
    edited html to have new playerID text box
    
    tried adding server.send feature but couldn't get it to work may need to add server code/modify html send function/ add a getPaddleLocation function for composite.

---
+++ cmake-build-debug/CMakeFiles/Ping_Pong.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/MattSparks/Documents/GitHub/Ping-Pong/Ping Pong/Ball.cpp" "/Users/MattSparks/Documents/GitHub/Ping-Pong/cmake-build-debug/CMakeFiles/Ping_Pong.dir/Ping_Pong/Ball.cpp.o"
+  "/Users/MattSparks/Documents/GitHub/Ping-Pong/Ping Pong/Bound.cpp" "/Users/MattSparks/Documents/GitHub/Ping-Pong/cmake-build-debug/CMakeFiles/Ping_Pong.dir/Ping_Pong/Bound.cpp.o"
+  "/Users/MattSparks/Documents/GitHub/Ping-Pong/Ping Pong/Communication Protocol.cpp" "/Users/MattSparks/Documents/GitHub/Ping-Pong/cmake-build-debug/CMakeFiles/Ping_Pong.dir/Ping_Pong/Communication_Protocol.cpp.o"
+  "/Users/MattSparks/Documents/GitHub/Ping-Pong/Ping Pong/Game Engine.cpp" "/Users/MattSparks/Documents/GitHub/Ping-Pong/cmake-build-debug/CMakeFiles/Ping_Pong.dir/Ping_Pong/Game_Engine.cpp.o"
+  "/Users/MattSparks/Documents/GitHub/Ping-Pong/Ping Pong/Paddle.cpp" "/Users/MattSparks/Documents/GitHub/Ping-Pong/cmake-build-debug/CMakeFiles/Ping_Pong.dir/Ping_Pong/Paddle.cpp.o"
+  "/Users/MattSparks/Documents/GitHub/Ping-Pong/Ping Pong/main.cpp" "/Users/MattSparks/Documents/GitHub/Ping-Pong/cmake-build-debug/CMakeFiles/Ping_Pong.dir/Ping_Pong/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Ping_Pong.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ping_Pong.dir/Ping_Pong/Ball.cpp.o"
+  "CMakeFiles/Ping_Pong.dir/Ping_Pong/Bound.cpp.o"
+  "CMakeFiles/Ping_Pong.dir/Ping_Pong/Communication_Protocol.cpp.o"
+  "CMakeFiles/Ping_Pong.dir/Ping_Pong/Game_Engine.cpp.o"
+  "CMakeFiles/Ping_Pong.dir/Ping_Pong/main.cpp.o"
+  "CMakeFiles/Ping_Pong.dir/Ping_Pong/Paddle.cpp.o"
+  "Ping_Pong.pdb"
+  "Ping_Pong"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ping_Pong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
